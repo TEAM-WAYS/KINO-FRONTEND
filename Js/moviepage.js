e73837fb8d03654772378aa7579ef981a90f1d3f
YmerA38 <stin166c@stud.kea.dk>
2023-10-05 09:56:09 +0200

    movisite

--- Js/moviepage.js
+++ Js/moviepage.js
@@ -3,11 +3,28 @@ let title = sessionStorage.getItem("title")
 const table = document.getElementById("table")
 //const row = document.getElementById("row")
 const pbGoBack = document.getElementById("pbGoBack")
+const hall = fetchhalls()
+
+let row
+let cellCount = 0
+function setHeader(){
+
+    row = table.insertRow(0)
+    halls.forEach(function (hall) {cell = row.insertCell(cellCount++) })
+    cell.appendChild(hall.name)
+    cellCount = 0
+}
 
 function setTable(timeSlot) {
-    let cellCount = 0
+
+    rowCount = table.rows.length
+    if(cellCount%halls.length == 0) {
+        row = table.insertRow(rowCount)
+        cellCount = 0
+    }
+
     let rowCount = table.rows.length
-    let row = table.insertRow(rowCount)
+    row = table.insertRow(rowCount)
     
 
     cell = row.insertCell(cellCount++)
@@ -24,14 +41,23 @@ async function fetchTimeslots(){
     timeslots = await fetchAnyUrl(urlTimeslots)
     timeslots.forEach(setTable)
 }
+async function fetchhalls(){
+    const halls = await fetchAnyUrl(urlHalls)
+    return halls
+}
+
+
 function fetchAnyUrl(url) {
     return fetch(url).then(response => response.json())
 }
 
-fetchTimeslots()
+
 
 pbGoBack.addEventListener("click", function(){goBack()})
 
 function goBack(){
     window.location.href = "frontpage.html"
 }
+
+setHeader()
+fetchTimeslots()
