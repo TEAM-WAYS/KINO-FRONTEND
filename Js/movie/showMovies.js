1bb12800c6cd40a066b9a10f0db9601650ac5804
AmingYee <wenm0007@stud.kea.dk>
2023-10-10 10:44:11 +0200

    movie stuff not done create/edit/delete done

---
+++ Js/movie/showMovies.js
@@ -0,0 +1,75 @@
+document.addEventListener("DOMContentLoaded", () => {
+    fetch("http://localhost:8080/movies")
+        .then(response => response.json())
+        .then(data => {
+            const movieList = document.getElementById("movieList");
+            data.forEach(movie => {
+                const movieDiv = document.createElement("div");
+                movieDiv.className = "movie";
+
+                const title = document.createElement("h2");
+                title.textContent = movie.title;
+
+                const duration = document.createElement("p");
+                duration.textContent = `Duration: ${movie.duration} minutes`;
+
+                const director = document.createElement("p");
+                director.textContent = `Director: ${movie.director}`;
+
+                const image = document.createElement("img");
+                image.src = movie.image;
+                image.alt = movie.title;
+
+                image.style.width = "200px";
+                image.style.height = "300px";
+
+                const genre = document.createElement("p");
+                genre.textContent = `Genre: ${movie.genre}`;
+
+                const description = document.createElement("p");
+                description.textContent = `Description: ${movie.description}`;
+
+                const pegi = document.createElement("p");
+                pegi.textContent = `Pegi: ${movie.pegi}`;
+
+                const editButton = document.createElement("a");
+                editButton.textContent = "Edit";
+                editButton.href = `editMovie.html?id=${movie.id}`;
+
+                const deleteButton = document.createElement("a");
+                deleteButton.textContent = "Delete";
+                deleteButton.addEventListener("click", () => {
+                    const confirmDelete = window.confirm("Are you sure you want to delete this movie?");
+
+                    if (confirmDelete) {
+                        fetch(`http://localhost:8080/movies/delete/${movie.id}`, {
+                            method: "DELETE",
+                        })
+                            .then(response => {
+                                if (!response.ok) {
+                                    throw new Error("Delete request failed");
+                                }
+                                movieDiv.remove();
+                            })
+                            .catch(error => {
+                                console.error("Error deleting movie:", error);
+                            });
+                    }
+                });
+
+                movieDiv.appendChild(title);
+                movieDiv.appendChild(duration);
+                movieDiv.appendChild(director);
+                movieDiv.appendChild(image);
+                movieDiv.appendChild(genre);
+                movieDiv.appendChild(description);
+                movieDiv.appendChild(pegi);
+                movieDiv.appendChild(editButton);
+                movieDiv.appendChild(deleteButton);
+                movieList.appendChild(movieDiv);
+            });
+        })
+        .catch(error => {
+            console.error("Error fetching movies:", error);
+        });
+});
